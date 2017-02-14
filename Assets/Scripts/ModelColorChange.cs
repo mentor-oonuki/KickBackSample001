245b3deda5c71c5a4f4445bd13c99c860c5510c2
mentor-oonuki <mentor.oonuki@gmail.com>
2017-02-15 01:46:25 +0900

    【追加】 Modelの色を変化させる

---
+++ Assets/Scripts/ModelColorChange.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ModelColorChange : MonoBehaviour
+{
+
+    // モデルのマテリアルの色を変更する
+    public void ColorChange(Color color)
+    {
+        Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer>();
+
+        foreach(Renderer renderer in renderers)
+        {
+            renderer.material.color = color;
+        }
+
+    }
+
+    private void Start()
+    {
+        ColorChange(new Color(0.5f, 0.0f, 0.0f));
+    }
+
+}
